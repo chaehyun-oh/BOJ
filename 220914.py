cba58569f12ce7dce0da0faf04757d5e109ef472
chaehyun_oh <ohchaehh@gmail.com>
2022-09-14 23:52:10 +0900

    220914 문제풀이

---
+++ 220914.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('input01.txt')
+
+# 11650 좌표 정렬하기
+n = int(sys.stdin.readline())
+so = []
+for i in range(n):
+    so.append(list(map(int, sys.stdin.readline().split())))
+so.sort(key=lambda x: (x[0], x[1]))
+for i in so:
+    print(i[0], i[1])
